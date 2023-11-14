13ec805892881064854c748a2140325d3fbcc177
Edward <edward.bock@palasthotel.de>
2023-11-14 12:53:17 +0100

    fix things

--- jest.config.ts
+++ jest.config.ts
@@ -4,26 +4,6 @@
  */
 
 export default {
-
-    clearMocks: true,
-
-    coverageDirectory: "coverage",
-
-    coverageProvider: "v8",
-
-    roots: [
-        "<rootDir>/src"
-    ],
-
+    preset: "ts-jest",
     testEnvironment: "node",
-
-    testMatch: [
-        "**/__tests__/**/*.+(ts|tsx|js)",
-        "**/?(*.)+(spec|test).+(ts|tsx|js)"
-    ],
-
-    transform: {
-        "^.+\\.(ts|tsx)$": "ts-jest"
-    },
-
 };
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+
 // https://vitejs.dev/guide/build.html#library-mode
+
 export default defineConfig({
     build: {
         lib: {
