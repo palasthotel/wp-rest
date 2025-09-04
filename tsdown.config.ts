68cae328a710572ab4c369f1affd341a09db5d7b
Arne Seemann <arne.seemann@palasthotel.de>
2025-09-04 17:21:16 +0200

    tsdown config: remove unsopported tsup option 'splitting'

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
     target: 'es2020',
     format: ['cjs', 'esm', 'iife'],
-    splitting: false,
     sourcemap: true,
     clean: true,
     dts: true,
